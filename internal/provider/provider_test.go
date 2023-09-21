43f89e7c052bd5c0a87f8ad57893f948ddd957fb
Johan Svensson <johan@reducer.io>
2023-09-21 22:04:23 +0200

    test: add starting scaffold for provider tests (#16)

---
+++ internal/provider/provider_test.go
@@ -0,0 +1,15 @@
+package provider_test
+
+import (
+	"github.com/hashicorp/terraform-plugin-framework/providerserver"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+	"github.com/jsvensson/terraform-provider-detectify/internal/provider"
+)
+
+// testAccProtoV6ProviderFactories are used to instantiate a provider during
+// acceptance testing. The factory function will be invoked for every Terraform
+// CLI command executed to create a provider server to which the CLI can
+// reattach.
+var testAccProtoV6ProviderFactories = map[string]func() (tfprotov6.ProviderServer, error){
+	"detectify": providerserver.NewProtocol6WithError(provider.New("test")()),
+}
